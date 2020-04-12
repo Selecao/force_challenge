bb5960c7f3e317a7b8707a764490821452f761db
Odysseus <vaanneko@gmail.com>
2020-04-12 20:23:10 +0300

    Continue making layout.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'constants.dart' as constants;
-import 'widgets/custom_sliver_message.dart';
-import 'widgets/networking_page_header.dart';
+import 'force_home_page.dart';
 
 void main() {
   runApp(ForceChallengeApp());
@@ -22,95 +20,6 @@ class ForceChallengeApp extends StatelessWidget {
         ),
       ),
       home: ForceHomePage(),
-    );
-  }
-}
-
-class ForceHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverPersistentHeader(
-            pinned: true,
-            floating: true,
-            delegate: NetworkingPageHeader(
-              minExtent: 100.0,
-              maxExtent: 250.0,
-            ),
-          ),
-          CustomSliverMessage(),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: FlatButton(
-                  color: constants.buttonColor,
-                  padding: const EdgeInsets.all(12.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: constants.framesRadius,
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    'Загрузить уведомления',
-                    style: constants.defaultTextStyle,
-                  )),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Последние',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.orange[100 * (index % 9)],
-                  child: Text(
-                    'orange $index',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                );
-              },
-              childCount: 9,
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Ранее',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.lightBlue[100 * (index % 9)],
-                  child: Text('List Item $index'),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/models/message.dart
@@ -0,0 +1,7 @@
+class Message {
+  const Message({this.unread, this.imageUrl, this.text, this.price});
+  final bool unread;
+  final String imageUrl;
+  final String text;
+  final int price;
+}
