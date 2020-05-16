9515705a8aa1da0d7bae6bb7367479c2e5f4bc23
Odysseus <vaanneko@gmail.com>
2020-05-16 19:36:46 +0300

    refactor code and remove one import

--- lib/force_home_page.dart
+++ lib/force_home_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:forcechallenge/widgets/message_sliver_list.dart';
+import 'package:forcechallenge/widgets/page_sliver_header.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
 
+import 'package:forcechallenge/constants.dart' as constants;
 import 'package:forcechallenge/state/message_store.dart';
-import 'package:forcechallenge/widgets/message_items_view.dart';
 
 class ForceHomePage extends StatefulWidget {
   @override
@@ -44,7 +47,179 @@ class _ForceHomePageState extends State<ForceHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MessageItemsView(_messageStore),
+      body: Observer(
+        builder: (_) {
+          switch (_messageStore.status) {
+            case FutureStatus.pending:
+              return buildLoading();
+            case FutureStatus.fulfilled:
+              return buildLoaded();
+            default:
+              return buildInitialInput();
+          }
+        },
+      ),
     );
   }
+
+  Widget buildInitialInput() {
+    return CustomScrollView(slivers: [
+      SliverPersistentHeader(
+        pinned: true,
+        floating: true,
+        delegate: PageSliverHeader(
+          minExtent: 100.0,
+          maxExtent: 250.0,
+        ),
+      ),
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 40.0),
+                decoration: BoxDecoration(
+                  color: constants.greyLight,
+                  borderRadius: constants.framesRadius,
+                  boxShadow: [
+                    constants.boxShadow,
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Вы впервые?',
+                        style: constants.boldTextStyle,
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        'Это очень важное сообщение. Пожалуйста, нажмите на кнопку ниже.',
+                        textAlign: TextAlign.center,
+                        style: constants.defaultDarkTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      color: constants.buttonColor,
+                      padding: const EdgeInsets.all(12.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: constants.framesRadius,
+                      ),
+                      child: Text(
+                        'Загрузить уведомления',
+                        style: constants.defaultTextStyle,
+                      ),
+                      onPressed: _refresh,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    ]);
+  }
+
+  Widget buildLoading() {
+    return CustomScrollView(slivers: [
+      SliverPersistentHeader(
+        pinned: true,
+        floating: true,
+        delegate: PageSliverHeader(
+          minExtent: 100.0,
+          maxExtent: 250.0,
+        ),
+      ),
+      SliverFillRemaining(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircularProgressIndicator(),
+              SizedBox(height: 16.0),
+              Text('Загрузка сообщений...'),
+            ],
+          ),
+        ),
+      ),
+    ]);
+  }
+
+  Widget buildLoaded() {
+    return RefreshIndicator(
+      onRefresh: _refresh,
+      child: CustomScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        slivers: [
+          SliverPersistentHeader(
+            pinned: true,
+            floating: true,
+            delegate: PageSliverHeader(
+              minExtent: 100.0,
+              maxExtent: 250.0,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Последние',
+                style: constants.notificationTitleTextStyle,
+              ),
+            ),
+          ),
+          MessageSliverList(
+            backgroundColor: constants.greyLight,
+            notificationList: _messageStore.unreadMessages,
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Ранее',
+                style: constants.notificationTitleTextStyle,
+              ),
+            ),
+          ),
+          MessageSliverList(
+            backgroundColor: null,
+            notificationList: _messageStore.readMessages,
+          ),
+          SliverToBoxAdapter(
+            child: SafeArea(
+              top: false,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Center(
+                  child: Text(
+                    'Это все сообщения!',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future _refresh() => _messageStore.fetchMessages();
 }
